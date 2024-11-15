9dad20519709f777e6a9b45518951ea56a54c8ef
un01s <96022876+un01s@users.noreply.github.com>
2024-11-15 10:56:14 -0700

    built-not-running

--- memory.x
+++ memory.x
@@ -1,7 +1,9 @@
 MEMORY {
-    BOOT2 : ORIGIN = 0x10000000, LENGTH = 0x100
-    FLASH : ORIGIN = 0x10000100, LENGTH = 2048K - 0x100
+    BOOT2 : ORIGIN = 0x10000000, LENGTH = 0x200
+    FLASH : ORIGIN = 0x10000200, LENGTH = 2048K - 0x200
     RAM   : ORIGIN = 0x20000000, LENGTH = 256K
+    SRAM4 : ORIGIN = 0x20040000, LENGTH = 4k
+    SRAM5 : ORIGIN = 0x20041000, LENGTH = 4k
 }
 
 EXTERN(BOOT2_FIRMWARE)
@@ -13,3 +15,38 @@ SECTIONS {
         KEEP(*(.boot2));
     } > BOOT2
 } INSERT BEFORE .text;
+
+SECTIONS {
+    /* ### Boot ROM info
+     *
+     * Goes after .vector_table, to keep it in the first 512 bytes of flash,
+     * where picotool can find it
+     */
+    .boot_info : ALIGN(4)
+    {
+        KEEP(*(.boot_info));
+    } > FLASH
+
+} INSERT AFTER .vector_table;
+
+/* move .text to start /after/ the boot info */
+_stext = ADDR(.boot_info) + SIZEOF(.boot_info);
+
+SECTIONS {
+    /* ### Picotool 'Binary Info' Entries
+     *
+     * Picotool looks through this block (as we have pointers to it in our
+     * header) to find interesting information.
+     */
+    .bi_entries : ALIGN(4)
+    {
+        /* We put this in the header */
+        __bi_entries_start = .;
+        /* Here are the entries */
+        KEEP(*(.bi_entries));
+        /* Keep this block a nice round size */
+        . = ALIGN(4);
+        /* We put this in the header */
+        __bi_entries_end = .;
+    } > FLASH
+} INSERT AFTER .text;
--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,21 @@
 //!
 //! Blinks the USER LED and NeoPixel RGB LED on a Seeed Studio XIAO RP2040 board.
 //!
+//! setup the change is to async both user LED and RGB LED in color
+//! change the control of both LEDs to different cores. RP2040 has two cores.
+//!
 
 #![no_std]
 #![no_main]
 
+use cortex_m::delay::Delay;
 use embedded_hal::digital::v2::OutputPin;
 use hal::pio::PIOExt;
 use hal::Timer;
 use panic_halt as _;
 use seeeduino_xiao_rp2040::entry;
 use seeeduino_xiao_rp2040::hal;
+use hal::multicore::{Multicore, Stack};
 use seeeduino_xiao_rp2040::hal::pac;
 use seeeduino_xiao_rp2040::hal::prelude::*;
 use smart_leds::SmartLedsWrite;
@@ -25,6 +30,28 @@ const RED: RGB8 = RGB8::new(LUMINOSITY, 0, 0);
 const GREEN: RGB8 = RGB8::new(0, LUMINOSITY, 0);
 const BLUE: RGB8 = RGB8::new(0, 0, LUMINOSITY);
 const WHITE: RGB8 = RGB8::new(LUMINOSITY / 3, LUMINOSITY / 3, LUMINOSITY / 3);
+
+/// second bootloader
+#[link_section = ".boot2"]
+#[used]
+pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_GENERIC_03H;
+
+///
+const XTAL_FREQ_HZ: u32 = 12_000_000u32;
+
+/// The frequency at which core 0 will blink its LED (Hz).
+const CORE0_FREQ: u32 = 1;
+/// The frequency at which core 1 will blink its LED (Hz).
+const CORE1_FREQ: u32 = 2;
+/// The delay between each toggle of core 0's LED (us).
+const CORE0_DELAY: u32 = 1_000_000 / CORE0_FREQ;
+/// The delay between each toggle of core 1's LED (us).
+const CORE1_DELAY: u32 = 1_000_000 / CORE1_FREQ;
+
+/// stack for core 1
+///
+/// core0 gets its stack via the normal route
+static mut CORE1_STACK: Stack<4096> = Stack::new();
 
 /// Entry point to our bare-metal application.
 ///
@@ -58,7 +85,7 @@ fn main() -> ! {
     .unwrap();
 
     // The single-cycle I/O block controls our GPIO pins
-    let sio = hal::Sio::new(pac.SIO);
+    let mut sio = hal::Sio::new(pac.SIO);
 
     // Set the pins up according to their function on this particular board
     let pins = seeeduino_xiao_rp2040::Pins::new(
@@ -94,29 +121,72 @@ fn main() -> ! {
     let mut led_green_pin = pins.led_green.into_push_pull_output();
     let mut led_red_pin = pins.led_red.into_push_pull_output();
 
+    // set up delay for the first core
+    let sys_freq = clocks.system_clock.freq().to_Hz();
+    //let mut delay = Delay::new(core.SYST, sys_freq);
+
+    // Start up the second core to blink the second LED
+    let mut mc = Multicore::new(&mut pac.PSM, &mut pac.PPB, &mut sio.fifo);
+    let cores = mc.cores();
+    let core1 = &mut cores[1];
+    #[allow(static_mut_refs)]
+    core1
+        .spawn(unsafe { &mut CORE1_STACK.mem }, move || {
+            // Get the second core's copy of the `CorePeripherals`, which are per-core.
+            // Unfortunately, `cortex-m` doesn't support this properly right now,
+            // so we have to use `steal`.
+            let core = unsafe { pac::CorePeripherals::steal() };
+            // Set up the delay for the second core.
+            let mut delay = Delay::new(core.SYST, sys_freq);
+            // Blink the User LED.
+            loop {
+                // Set USER LED to blue
+                led_blue_pin.set_low().unwrap();
+                led_red_pin.set_high().unwrap();
+                led_green_pin.set_high().unwrap();
+                delay.delay_us(CORE1_DELAY);
+
+                // Set USER LED to red
+                led_blue_pin.set_high().unwrap();
+                led_red_pin.set_low().unwrap();
+                led_green_pin.set_high().unwrap();
+                delay.delay_us(CORE1_DELAY);
+
+                // Set USER LED to green
+                led_blue_pin.set_high().unwrap();
+                led_red_pin.set_high().unwrap();
+                led_green_pin.set_low().unwrap();
+                delay.delay_us(CORE1_DELAY);
+            }
+        })
+        .unwrap();
+
     loop {
         // Set USER LED to blue
-        led_blue_pin.set_low().unwrap();
-        led_red_pin.set_high().unwrap();
-        led_green_pin.set_high().unwrap();
+        //led_blue_pin.set_low().unwrap();
+        //led_red_pin.set_high().unwrap();
+        //led_green_pin.set_high().unwrap();
         // Set RGB LED to blue
         ws.write([BLUE].iter().copied()).unwrap();
-        delay.delay_ms(1000);
+        //delay.delay_ms(1000);
+        delay.delay_us(CORE0_DELAY);
 
         // Set USER LED to red
-        led_blue_pin.set_high().unwrap();
-        led_red_pin.set_low().unwrap();
-        led_green_pin.set_high().unwrap();
+        //led_blue_pin.set_high().unwrap();
+        //led_red_pin.set_low().unwrap();
+        //led_green_pin.set_high().unwrap();
         // Set RGB LED to red
         ws.write([RED].iter().copied()).unwrap();
-        delay.delay_ms(1000);
+        //delay.delay_ms(1000);
+        delay.delay_us(CORE0_DELAY);
 
         // Set USER LED to green
-        led_blue_pin.set_high().unwrap();
-        led_red_pin.set_high().unwrap();
-        led_green_pin.set_low().unwrap();
+        //led_blue_pin.set_high().unwrap();
+        //led_red_pin.set_high().unwrap();
+        //led_green_pin.set_low().unwrap();
         // Set RGB LED to green
         ws.write([GREEN].iter().copied()).unwrap();
-        delay.delay_ms(1000);
+        //delay.delay_ms(1000);
+        delay.delay_us(CORE0_DELAY);
     }
 }
