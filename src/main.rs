ccae3dfebcda060bf66ccbe09ad898c28df2c3ec
un01s <96022876+un01s@users.noreply.github.com>
2024-12-12 10:04:23 -0700

    update

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,55 @@
-//! XIAO RP2040 Blinking LED Example
 //!
-//! https://tutoduino.fr/
-//!
-//! Blinks the USER LED and NeoPixel RGB LED on a Seeed Studio XIAO RP2040 board.
-//!
+//! XIAO RP2040 code for SF
+//! 2024-12-12
+//! 
+//! mapping first
 
 #![no_std]
 #![no_main]
 
-use embedded_hal::digital::v2::OutputPin;
-use hal::pio::PIOExt;
-use hal::Timer;
+#![allow(unused_variables)]
+
+//#[cfg(test)]
+//#[macro_use]
+//extern crate std;
+
+//use non_std::prelude::*;
+
+// The macro for our start-up function
+use rp_pico::entry;
+
+// Ensure we halt the program on panic (if we don't mention this crate it won't
+// be linked)
 use panic_halt as _;
-use seeeduino_xiao_rp2040::entry;
-use seeeduino_xiao_rp2040::hal;
-use seeeduino_xiao_rp2040::hal::pac;
-use seeeduino_xiao_rp2040::hal::prelude::*;
-use smart_leds::SmartLedsWrite;
-use smart_leds::RGB8;
+
+// Pull in any important traits
+use rp_pico::hal::prelude::*;
+
+// A shorter alias for the Peripheral Access Crate, which provides low-level
+// register access
+use rp_pico::hal::pac;
+
+// Import the Timer for Ws2812:
+use rp_pico::hal::timer::Timer;
+
+// A shorter alias for the Hardware Abstraction Layer, which provides
+// higher-level drivers.
+use rp_pico::hal;
+
+// PIOExt for the split() method that is needed to bring
+// PIO0 into useable form for Ws2812:
+use rp_pico::hal::pio::PIOExt;
+
+// Import useful traits to handle the ws2812 LEDs:
+use smart_leds::{brightness, SmartLedsWrite, RGB8};
+
+// Import the actual crate to handle the Ws2812 protocol:
 use ws2812_pio::Ws2812;
 
-const LUMINOSITY: u8 = 10;
-const RED: RGB8 = RGB8::new(LUMINOSITY, 0, 0);
-const GREEN: RGB8 = RGB8::new(0, LUMINOSITY, 0);
-const BLUE: RGB8 = RGB8::new(0, 0, LUMINOSITY);
+// Currently 3 consecutive LEDs are driven by this example
+// to keep the power draw compatible with USB:
+const LED_NUM: usize = 65; // number of LEDs
 
-/// Entry point to our bare-metal application.
-///
-/// The `#[entry]` macro ensures the Cortex-M start-up code calls this function
-/// as soon as all global variables are initialised.
-///
-/// The function configures the RP2040 peripherals, then blinks the LED in an
-/// infinite loop.
 #[entry]
 fn main() -> ! {
     // Grab our singleton objects
@@ -43,9 +61,9 @@ fn main() -> ! {
 
     // Configure the clocks
     //
-    // The default is to generate a 125 MHz system clock from 12 Mhz crystal
+    // The default is to generate a 125 MHz system clock
     let clocks = hal::clocks::init_clocks_and_plls(
-        seeeduino_xiao_rp2040::XOSC_CRYSTAL_FREQ,
+        rp_pico::XOSC_CRYSTAL_FREQ,
         pac.XOSC,
         pac.CLOCKS,
         pac.PLL_SYS,
@@ -60,62 +78,205 @@ fn main() -> ! {
     let sio = hal::Sio::new(pac.SIO);
 
     // Set the pins up according to their function on this particular board
-    let pins = seeeduino_xiao_rp2040::Pins::new(
+    let pins = rp_pico::Pins::new(
         pac.IO_BANK0,
         pac.PADS_BANK0,
         sio.gpio_bank0,
         &mut pac.RESETS,
     );
 
-    // The delay object lets us wait for specified amounts of time (in
-    // milliseconds)
-    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
-    let timer = Timer::new(pac.TIMER, &mut pac.RESETS);
+    // Setup a delay for the LED blink signals:
+    let mut frame_delay =
+        cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
 
-    // Setup PIO
+    // Import the `sin` function for a smooth hue animation from the
+    // Pico rp2040 ROM:
+    let sin = hal::rom_data::float_funcs::fsin::ptr();
+
+    // Create a count down timer for the Ws2812 instance:
+    let timer = Timer::new(pac.TIMER, &mut pac.RESETS, &clocks);
+
+    // Split the PIO state machine 0 into individual objects, so that
+    // Ws2812 can use it:
     let (mut pio, sm0, _, _, _) = pac.PIO0.split(&mut pac.RESETS);
 
-    // Setup Neopixel RGB LED
+    // Instanciate a Ws2812 LED strip
+    // GPIO27 is the data input to the LEDs of WS2812B-2020
     let mut ws = Ws2812::new(
-        pins.neopixel_data.into_mode(),
+        pins.gpio27.into_function(),
         &mut pio,
         sm0,
         clocks.peripheral_clock.freq(),
         timer.count_down(),
     );
 
-    // Power on Neopixel RGB LED
-    let mut neopixel_power = pins.neopixel_power.into_push_pull_output();
-    neopixel_power.set_high().unwrap();
+    let mut leds: [RGB8; LED_NUM] = [(0, 0, 0).into(); LED_NUM];
+    let mut t: f32 = 0.0;
+    let mut hue_offs: f32;
 
-    // Configure the USER LED pins to operate as a push-pull output
-    let mut led_blue_pin = pins.led_blue.into_push_pull_output();
-    let mut led_green_pin = pins.led_green.into_push_pull_output();
-    let mut led_red_pin = pins.led_red.into_push_pull_output();
+    // Bring down the overall brightness of the strip to not blow
+    // the USB power supply: every LED draws ~60mA, RGB means 3 LEDs per
+    // ws2812 LED, for 3 LEDs that would be: 3 * 3 * 60mA, which is
+    // already 540mA for just 3 white LEDs!
+    let strip_brightness = 64u8; // Limit brightness to 64/256
 
+    // Slow down timer by this factor (0.1 will result in 10 seconds):
+    let animation_speed = 0.1;
+
+    let mut fcnt = 0;
     loop {
-        // Set USER LED to blue
-        led_blue_pin.set_low().unwrap();
-        led_red_pin.set_high().unwrap();
-        led_green_pin.set_high().unwrap();
-        // Set RGB LED to blue
-        ws.write([BLUE].iter().copied()).unwrap();
-        delay.delay_ms(500);
+        fcnt += 1;
 
-        // Set USER LED to red
-        led_blue_pin.set_high().unwrap();
-        led_red_pin.set_low().unwrap();
-        led_green_pin.set_high().unwrap();
-        // Set RGB LED to red
-        ws.write([RED].iter().copied()).unwrap();
-        delay.delay_ms(500);
+        for (i, led) in leds.iter_mut().enumerate() {
+            if fcnt > 0 && fcnt <= 1000 {
+              hue_offs = match i % LED_NUM {
+                1 => 0.2,
+                2 => 0.2,
+                3 => 0.2,
+                4 => 0.2,
+                5 => 0.2,
+                6 => 0.2,
+                _ => 0.5,
+              };
+            } else if fcnt > 1000 && fcnt <= 2000 {
+              hue_offs = match i % LED_NUM {
+                0 => 0.0,
+                1 => 0.1,
+                2 => 0.1,
+                3 => 0.1,
+                4 => 0.1,
+                5 => 0.1,
+                6 => 0.1,
+                9 => 0.2,
+                12 => 0.2,
+                15 => 0.2,
+                18 => 0.2,
+                21 => 0.2,
+                24 => 0.2,
+                _ => 0.05,
+              };
+            } else if fcnt > 2000 && fcnt <= 4000 {
+              hue_offs = match i % LED_NUM {
+                0 => 0.0,
+                1 => 0.053,
+                2 => 0.106,
+                3 => 0.159,
+                4 => 0.212,
+                5 => 0.265,
+                6 => 0.318,
+                9 => 0.371,
+                12 => 0.424,
+                15 => 0.477,
+                18 => 0.53,
+                21 => 0.583,
+                24 => 0.636,
+                8 => 0.689,
+                11 => 0.742,
+                14 => 0.795,
+                17 => 0.848,
+                20 => 0.901,
+                23 => 0.954,
+                _ => 1.0,
+              };
+            } else if fcnt > 4000 && fcnt <= 6000 {
+              hue_offs = match i % LED_NUM {
+                0 => 0.083,
+                7 => 0.083,
+                8 => 0.083,
+                9 => 0.083,
+                10 => 0.167,
+                11 => 0.167,
+                12 => 0.167,
+                13 => 0.250,
+                14 => 0.250,
+                15 => 0.250,
+                16 => 0.333,
+                17 => 0.333,
+                18 => 0.333,
+                19 => 0.416,
+                20 => 0.416,
+                21 => 0.416,
+                22 => 0.499,
+                23 => 0.499,
+                24 => 0.499,
+                _ => 0.582,
+              };
+            } else {
+              hue_offs = (i/LED_NUM) as f32;
 
-        // Set USER LED to green
-        led_blue_pin.set_high().unwrap();
-        led_red_pin.set_high().unwrap();
-        led_green_pin.set_low().unwrap();
-        // Set RGB LED to green
-        ws.write([GREEN].iter().copied()).unwrap();
-        delay.delay_ms(500);
+              if fcnt > 7000 {
+                fcnt = 0;
+              }
+            }
+
+            if fcnt == 0 {
+              let hue = 360.0 * hue_offs;
+              let sat = 1.0;
+              let val = 1.0;
+
+              let rgb = hsv2rgb_u8(hue, sat, val);
+              *led = rgb.into();
+            } else {
+              let sin_11 = sin((t + hue_offs) * 2.0 * core::f32::consts::PI);
+              // Bring -1..1 sine range to 0..1 range:
+              let sin_01 = (sin_11 + 1.0) * 0.5;
+
+              let hue = 360.0 * sin_01;
+              let sat = 1.0;
+              let val = 1.0;
+
+              let rgb = hsv2rgb_u8(hue, sat, val);
+              *led = rgb.into();
+            }
+        }
+        
+        //pattern1(leds, t);
+
+        // Here the magic happens and the `leds` buffer is written to the
+        // ws2812 LEDs:
+        ws.write(brightness(leds.iter().copied(), strip_brightness))
+            .unwrap();
+
+        // Wait a bit until calculating the next frame:
+        frame_delay.delay_ms(16); // ~60 FPS
+
+        // Increase the time counter variable and make sure it
+        // stays inbetween 0.0 to 1.0 range:
+        t += (16.0 / 1000.0) * animation_speed;
+        while t > 1.0 {
+            t -= 1.0;
+        }
     }
+}
+
+pub fn hsv2rgb(hue: f32, sat: f32, val: f32) -> (f32, f32, f32) {
+    let c = val * sat;
+    let v = (hue / 60.0) % 2.0 - 1.0;
+    let v = if v < 0.0 { -v } else { v };
+    let x = c * (1.0 - v);
+    let m = val - c;
+    let (r, g, b) = if hue < 60.0 {
+        (c, x, 0.0)
+    } else if hue < 120.0 {
+        (x, c, 0.0)
+    } else if hue < 180.0 {
+        (0.0, c, x)
+    } else if hue < 240.0 {
+        (0.0, x, c)
+    } else if hue < 300.0 {
+        (x, 0.0, c)
+    } else {
+        (c, 0.0, x)
+    };
+    (r + m, g + m, b + m)
+}
+
+pub fn hsv2rgb_u8(h: f32, s: f32, v: f32) -> (u8, u8, u8) {
+    let r = hsv2rgb(h, s, v);
+
+    (
+        (r.0 * 255.0) as u8,
+        (r.1 * 255.0) as u8,
+        (r.2 * 255.0) as u8,
+    )
 }
